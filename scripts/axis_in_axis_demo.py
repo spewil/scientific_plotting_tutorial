4461f735f997db83d7e648d392c35ca167d2ef74
spewil <spencerryanwilson@gmail.com>
2020-03-11 10:06:08 +0000

    100 axes demo

--- scripts/axis_in_axis_demo.py
+++ scripts/axis_in_axis_demo.py
@@ -3,14 +3,23 @@ matplotlib.use("Qt5Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 20, 1000)
-y1 = np.sin(x)
-y2 = np.cos(x)
+# 100 Axes objects
 
-fig = plt.figure()
-ax = fig.add_subplot()
-ax.plot(x, y1, "-b", label="sine")
-ax.plot(x, y2, "-r", label="cosine")
-ax.legend(loc="upper left", n)
-ax.set_ylim(-1.5, 2.0)
+x = np.linspace(0, 20, 1000)
+y = np.sin(x)
+
+fig = plt.figure(1)
+
+axes = []
+for _ in range(100):
+    ax = plt.Axes(
+        fig=fig,
+        rect=[
+            np.random.uniform(high=0, low=1),
+            np.random.uniform(high=0, low=1), 0.25, 0.25
+        ])
+    ax.plot(x, y, "-b", label="sine")
+    axes.append(ax)
+    fig.add_axes(ax)
+
 plt.show()
--- scripts/colorbar_demo.py
+++ scripts/colorbar_demo.py
@@ -4,11 +4,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 plt.style.use('seaborn-dark')
 
-fig = plt.figure(1)
-ax = fig.add_subplot(211)
-img_plot = ax.imshow(np.random.random((100, 100)), vmin=.5)
-(x0, y0, w, h) = img_plot.get_window_extent()
-print(x0, y0, w, h)
-cax = fig.add_axes([x0, y0, 0.05, h])
-cbar = fig.colorbar(img_plot, cax=cax)
+fig = plt.figure(2)
+ax = fig.add_subplot(111)
+img_plot = ax.imshow(np.random.random((100, 100)), vmin=0, vmax=1)
+
+fig.subplots_adjust(bottom=0, top=1, left=0, right=.9)
+
+cb_ax = fig.add_axes([0.87, 0.1, 0.05, 0.865])
+cbar = fig.colorbar(img_plot, cax=cb_ax)
+
+# set the colorbar ticks and tick labels
+cbar.set_ticks(np.linspace(0, 1, 3, endpoint=True))
+cbar.set_ticklabels(['low', 'medium', 'high'])
+fig.tight_layout()
 plt.show()
